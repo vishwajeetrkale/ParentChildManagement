fbc8dba6220a51d102bc3bcf20c1b6d7ffe9b21e
vishwajeet <vishwajeetrkale@gmail.com>
2017-08-29 19:27:34 +0530

    1st project check in

---
+++ ParentChildManagementCodeFirstProject/ParentChildManagementCodeFirstProject/Models/ChildDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ParentChildManagementCodeFirstProject.Models
+{
+    public class ChildDetail
+    {
+        public int Id { get; set; }
+
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        public int ParentId {get; set;}
+    }
+}
---
+++ ParentChildManagementCodeFirstProject/ParentChildManagementCodeFirstProject/Models/ParentDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ParentChildManagementCodeFirstProject.Models
+{
+    public class ParentDetail
+    {
+
+        public int Id { get; set; }
+
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        public int? NumberOfChild { get; set; }
+
+    }
+}

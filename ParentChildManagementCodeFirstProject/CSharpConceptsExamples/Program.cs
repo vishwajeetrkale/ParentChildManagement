54c82360a1483deb087a1b7581077b50f0b2d16f
vishwajeet <vishwajeetrkale@gmail.com>
2017-10-03 21:28:32 +0530

    3 Oct 2017

--- ParentChildManagementCodeFirstProject/CSharpConceptsExamples/Program.cs
+++ ParentChildManagementCodeFirstProject/CSharpConceptsExamples/Program.cs
@@ -10,6 +10,25 @@ namespace CSharpConceptsExamples
     {
         static void Main(string[] args)
         {
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+            BaseClass bcdc = new DerivedClass();
+
+            bc.Method1();
+            dc.Method1();
+            dc.Method2();
+            bcdc.Method1();
+
+
+            BaseC bac = new BaseC();
+            DerivedC dec = new DerivedC();
+            BaseC badec = new DerivedC();
+            badec.Invoke
         }
+        // Output:  
+        // Base - Method1  
+        // Base - Method1  
+        // Derived - Method2  
+        // Base - Method1
     }
 }
---
+++ ParentChildManagementCodeFirstProject/CSharpConceptsExamples/WhenToUseOverrideAndNewKeywords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConceptsExamples
+{
+    class WhenToUseOverrideAndNewKeywords
+    {
+    }
+
+    class BaseClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Base - Method1");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public void Method2()
+        {
+            Console.WriteLine("Derived - Method2");
+        }
+    }
+
+    public class BaseC
+    {
+        public int x;
+        public virtual void Invoke() { }
+    }
+
+    public class DerivedC : BaseC
+    {
+        public override void Invoke() { }
+    }
+
+}
